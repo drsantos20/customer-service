de9eea7d2fd080ace560825e40ba8c03e9f417da
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-17 01:15:59 -0300

    Add user model

--- api/order/factories.py
+++ api/order/factories.py
@@ -1,14 +1,31 @@
 import factory
 
 from api.order.models import UserOrderAddress
+from api.order.models.user import User
+from api.order.models.order import UserOrder
 
 
 class UserOrderAddressFactory(factory.DjangoModelFactory):
     street = factory.Faker('pystr')
     city = factory.Faker('pystr')
     neighborhood = factory.Faker('pystr')
-    zip_code = factory.Faker('pystr')
-    uf = factory.Faker('pystr')
 
     class Meta:
         model = UserOrderAddress
+
+
+class UserFactory(factory.DjangoModelFactory):
+    name = factory.Faker('pystr')
+    email = factory.Faker('pystr')
+    phone = factory.Faker('pyint')
+
+    class Meta:
+        model = User
+
+
+class UserOrderSerializerFactory(factory.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    address = factory.SubFactory(UserOrderAddressFactory)
+
+    class Meta:
+        model = UserOrder
--- api/order/models/__init__.py
+++ api/order/models/__init__.py
@@ -1,1 +1,3 @@
 from .address import UserOrderAddress
+from .order import UserOrder
+from .user import User
