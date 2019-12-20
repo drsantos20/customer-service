c9a10cf61d0f5dd8c9dfaec201287588a8e8637d
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-20 01:00:20 -0300

    Add table name and add latitude and longitude fields

--- api/order/models/address.py
+++ api/order/models/address.py
@@ -5,5 +5,10 @@ class UserOrderAddress(models.Model):
     street = models.CharField(max_length=150, null=False, blank=False)
     city = models.CharField(max_length=150, null=False, blank=False)
     neighborhood = models.CharField(max_length=150, null=False, blank=False)
-    zip_code = models.CharField(max_length=12)
-    uf = models.CharField(max_length=12)
+    zip_code = models.CharField(max_length=12, null=False, blank=False)
+    uf = models.CharField(max_length=12, null=False, blank=False)
+    latitude = models.CharField(max_length=30, null=True, blank=True)
+    longitude = models.CharField(max_length=30, null=True, blank=True)
+
+    class Meta:
+        db_table = 'address'
