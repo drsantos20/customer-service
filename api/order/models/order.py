0f3e7275f1e62d2fe608c6d560ceb3c13e841324
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-20 00:58:47 -0300

    Remove app prefix from tables

--- api/order/models/order.py
+++ api/order/models/order.py
@@ -6,3 +6,6 @@ from api.order.models import UserOrderAddress
 class UserOrder(models.Model):
     user = models.ForeignKey('order.User', on_delete=models.CASCADE)
     address = models.ForeignKey(UserOrderAddress, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'order'
--- api/order/models/user.py
+++ api/order/models/user.py
@@ -5,3 +5,6 @@ class User(models.Model):
     name = models.CharField(max_length=100, null=False)
     email = models.EmailField(max_length=50, null=True)
     phone = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'user'
