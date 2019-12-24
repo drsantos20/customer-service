150058a1c6e4be5e9618e61e8be38e79a582e68d
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-24 00:30:47 -0300

    Add validation to check if the request address already exists on database and avoid producer call if lat and long is already present on address data

--- api/order/serializers/order_serializer.py
+++ api/order/serializers/order_serializer.py
@@ -22,19 +22,26 @@ class UserOrderSerializer(serializers.ModelSerializer):
 
     @transaction.atomic
     def create(self, validated_data):
-        address = UserOrderAddress.objects.create(
-            street=validated_data['address']['street'],
-            city=validated_data['address']['city'],
-            neighborhood=validated_data['address']['neighborhood'],
-            zip_code=validated_data['address']['zip_code'],
-            uf=validated_data['address']['uf']
-        )
+        address = UserOrderAddress.objects.filter(street=validated_data['address']['street'])
+
+        if address.exists():
+            address_from_order = address.first()
+        else:
+            address_from_order = UserOrderAddress.objects.create(
+                street=validated_data['address']['street'],
+                city=validated_data['address']['city'],
+                neighborhood=validated_data['address']['neighborhood'],
+                zip_code=validated_data['address']['zip_code'],
+                uf=validated_data['address']['uf']
+            )
 
         user, created = User.objects.get_or_create(
             email=validated_data['user']['email'],
             name=validated_data['user']['name'],
             phone=validated_data['user']['phone'],
         )
-        order = UserOrder.objects.create(address=address, user=user)
-        publish_metadata(message=order.address)
+        order = UserOrder.objects.create(address=address_from_order, user=user)
+
+        if not address_from_order.latitude and not address_from_order.longitude:
+            publish_metadata(message=order.address)
         return order
