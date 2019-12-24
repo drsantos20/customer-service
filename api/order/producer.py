7f57a386b8c8c042b7d189c313feca1de5e7ea72
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-24 17:56:19 -0300

    Update broker connection with surrounding context

--- api/order/producer.py
+++ api/order/producer.py
@@ -1,37 +1,35 @@
 from django.conf import settings
 from kombu import Connection, Exchange, Producer, Queue
 
-
-def publish_metadata(message):
-    connection = Connection(settings.BROKER_URL)
-    connection.connect()
-    channel = connection.channel()
-
-    exchange = Exchange('example-exchange', type='direct')
-
-    producer = Producer(
-        channel=channel,
-        routing_key='address',
-        exchange=exchange,
-    )
-    queue = Queue(
-        name='order-customer-queue',
-        routing_key='address',
-        exchange=exchange,
-    )
-
-    from api.order.serializers import UserOrderAddressSerializer
-    address_serializer = UserOrderAddressSerializer(message).data
-
-    queue.maybe_bind(connection)
-    queue.declare()
-    producer.publish(address_serializer)
-    connection.close()
+from api.order.constants import (
+    ADDRESS_EXCHANGE,
+    ADDRESS_PRODUCER_ROUTING_KEY,
+    ADDRESS_PRODUCER_QUEUE,
+)
 
 
-def process_address(message):
-    from api.order.models import UserOrderAddress
-    address = UserOrderAddress.objects.get(id=message.get('id'))
-    address.latitude = message.get('lat')
-    address.longitude = message.get('lng')
-    address.save()
+def send_address_to_queue(message):
+    with Connection(settings.BROKER_URL) as connection:
+        connection.connect()
+        channel = connection.channel()
+
+        exchange = Exchange(ADDRESS_EXCHANGE, type='direct')
+
+        producer = Producer(
+            channel=channel,
+            routing_key=ADDRESS_PRODUCER_ROUTING_KEY,
+            exchange=exchange,
+        )
+        queue = Queue(
+            name=ADDRESS_PRODUCER_QUEUE,
+            routing_key=ADDRESS_PRODUCER_ROUTING_KEY,
+            exchange=exchange,
+        )
+
+        from api.order.serializers import UserOrderAddressSerializer
+        address_serializer = UserOrderAddressSerializer(message).data
+
+        queue.maybe_bind(connection)
+        queue.declare()
+        producer.publish(address_serializer)
+        connection.close()
